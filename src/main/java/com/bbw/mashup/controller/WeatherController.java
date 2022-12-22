29fcd486310582801263d554cbb52234d05b7c24
asuhaze!! <bolligertabea@gmail.com>
2022-12-22 09:36:23 +0100

    added annotations for weather

--- src/main/java/com/bbw/mashup/controller/WeatherController.java
+++ src/main/java/com/bbw/mashup/controller/WeatherController.java
@@ -3,11 +3,13 @@ package com.bbw.mashup.controller;
 import com.bbw.mashup.service.FlightService;
 import com.bbw.mashup.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Controller
 public class WeatherController {
     private WeatherService weatherService;
 
--- src/main/java/com/bbw/mashup/service/WeatherService.java
+++ src/main/java/com/bbw/mashup/service/WeatherService.java
@@ -3,10 +3,12 @@ package com.bbw.mashup.service;
 import com.bbw.mashup.model.Flights;
 import com.bbw.mashup.model.Weather;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class WeatherService {
 
     private WeatherRepository weatherRepository;
